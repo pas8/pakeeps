12efd622c25599b87d30d16cb69b487ecdd9c8d6
pas <whyiampas@gmail.com>
2021-04-26 01:09:05 +0300

    add Hover logic create great styling for this component and some refactor

--- src/components/IconsUtils/components/AddDateToPakeep/index.js
+++ src/components/IconsUtils/components/AddDateToPakeep/index.js
@@ -22,34 +22,42 @@ import EventNoteOutlinedIcon from '@material-ui/icons/EventNoteOutlined';
 import DateRangeOutlinedIcon from '@material-ui/icons/DateRangeOutlined';
 import ArrowBackOutlinedIcon from '@material-ui/icons/ArrowBackOutlined';
 import SaveOutlinedIcon from '@material-ui/icons/SaveOutlined';
+import { themeColors } from 'components/theme';
+import clsx from 'clsx';
+import shortid from 'shortid';
 
 const useStyles = makeStyles(theme => ({
   dateContainer: { padding: theme.spacing(0, 0) },
   // scale:{transform:'scale(0.88)'}
   itemGrid: { margin: theme.spacing(0.4, 1.4, 0, -0.8), padding: theme.spacing(0.8, 0) },
   menuText: { marginLeft: theme.spacing(1.4) },
-  marginTop: { marginTop: theme.spacing(-1) },
-  box: { marginBottom: theme.spacing(0.4), borderBottom: '1px solid rgba(255,255,255,0.4)' }
+  marginTop: { marginRight: theme.spacing(-0.8), marginLeft: theme.spacing(-0.4) },
+  box: { borderBottom: '1px solid rgba(255,255,255,0.4)' },
+  menuItemButton: { outline: 'none', background: 'none', border: 'none' }
 }));
 
 const AddDateToPakeep = () => {
+  const nullifyOfMenuItemState = { name: '', hoverStatus: false, clickStatus: false, changedStatus: false };
+
   const [selectedDate, handleDateChange] = useState(new Date());
+  const [buttonSaveState, setButtonSaveState] = useState(false);
+  const [menuItemState, setMenuItemState] = useState(nullifyOfMenuItemState);
+
   const classes = useStyles();
   const placeholderFunc = value => ({ value });
 
   const dateListArr = [
-    { title: 'Later today', icon: TodayOutlinedIcon, onClick: placeholderFunc },
-    { title: 'Tomorrow', icon: CalendarTodayOutlinedIcon, onClick: placeholderFunc },
-    { title: 'Next week', icon: ViewWeekOutlinedIcon, onClick: placeholderFunc },
-    { title: 'Add to dashboard', icon: DashboardOutlinedIcon, onClick: placeholderFunc },
+    { title: 'Later today', icon: TodayOutlinedIcon, onClick: placeholderFunc, name: 'laterToday' },
+    { title: 'Tomorrow', icon: CalendarTodayOutlinedIcon, onClick: placeholderFunc, name: 'tomorrow' },
+    { title: 'Next week', icon: ViewWeekOutlinedIcon, onClick: placeholderFunc, name: 'nextWeek' },
+    { title: 'Add to dashboard', icon: DashboardOutlinedIcon, onClick: placeholderFunc, name: 'addToDashboard' },
     {
       title: 'Add Date & Time',
       icon: EventNoteOutlinedIcon,
       onClick: placeholderFunc,
       dynamicComponent: (
-        <Grid item className={classes.marginTop}>
+        <Grid item className={clsx(classes.marginTop, classes.itemGrid)}>
           <KeyboardDateTimePicker
-            emptyLabel={''}
             value={selectedDate}
             onChange={handleDateChange}
             ampm={false}
@@ -61,45 +69,95 @@ const AddDateToPakeep = () => {
           />
         </Grid>
       ),
-      isDynamicComponentActive: true
-    },
-    { title: 'Add Location', icon: AddLocationOutlinedIcon, onClick: placeholderFunc },
-    { title: 'Add More Events', icon: DateRangeOutlinedIcon, onClick: placeholderFunc, hidden: !true }
-  ];
 
+      name: 'addDateAndTime'
+    },
+    { title: 'Add Location', icon: AddLocationOutlinedIcon, onClick: placeholderFunc, name: 'addLocation' },
+    {
+      title: 'Add More Events',
+      icon: DateRangeOutlinedIcon,
+      onClick: placeholderFunc,
+      hidden: true,
+      name: 'addMoreEvents'
+    }
+  ];
+  const setHoverOfMenuItemIsTrue = name => setMenuItemState(state => ({ ...state, name, hoverStatus: true }));
+
+  const setHoverOfMenuItemIsFalse = () => setMenuItemState(nullifyOfMenuItemState);
   return (
     <>
-      <Grid borderBottom={1} className={classes.box} display={'flex'} container>
-        <Grid flexGrow={1} item>
-          <IconButton>
-            <ArrowBackOutlinedIcon />
-          </IconButton>
-          <Typography variant={'subtitle1'}>Close</Typography>
+      <Grid borderBottom={1} className={classes.box} container justify={'space-between'}>
+        <Grid item>
+          <Grid container alignItems={'center'}>
+            <IconButton>
+              <ArrowBackOutlinedIcon />
+            </IconButton>
+            <Typography
+              variant={'subtitle1'}
+              style={{
+                color: 'rgba(255,255,255,0.8)'
+              }}
+            >
+              Close
+            </Typography>
+          </Grid>
         </Grid>
-        <SaveOutlinedIcon style={{ color: 'rgba(255,255,255,0.4)' }} />
+        <Grid item>
+          <IconButton>
+            <SaveOutlinedIcon
+              style={{
+                color:
+                  buttonSaveState === 'saved'
+                    ? themeColors.primaryMain
+                    : `rgba(255,255,255,${buttonSaveState ? 0.8 : 0.42}`
+              }}
+            />
+          </IconButton>
+        </Grid>
       </Grid>
-      {dateListArr.map(
-        ({ title, icon: Icon, onClick, hidden = false, dynamicComponent = false, isDynamicComponentActive }) => (
+
+      {dateListArr.map(({ title, icon: Icon, onClick, hidden = false, dynamicComponent = false, name }, idx) => {
+        let correctName = name === menuItemState.name;
+        let activeIcon = correctName && menuItemState.hoverStatus;
+        let onMouseEnterOfMenuItem = () => setHoverOfMenuItemIsTrue(name);
+
+        return (
           <>
             {!hidden ? (
-              <MenuItem>
-                <Grid className={classes.itemGrid} display={'flex'} container>
-                  <Icon />
-                  <Grid item className={classes.menuText}>
-                    {Boolean(dynamicComponent) && isDynamicComponentActive ? (
-                      dynamicComponent
-                    ) : (
-                      <>
-                        <Typography variant={'subtitle2'}> {title}</Typography>
-                      </>
-                    )}
-                  </Grid>
-                </Grid>
+              <MenuItem
+                key={shortid()}
+                onMouseEnter={onMouseEnterOfMenuItem}
+                onMouseLeave={setHoverOfMenuItemIsFalse}
+                onClick={onClick}
+              >
+                {Boolean(dynamicComponent) && correctName && menuItemState.clickStatus ? (
+                  dynamicComponent
+                ) : (
+                  <>
+                    <Grid className={clsx(classes.itemGrid)} container>
+                      <Icon
+                        style={{
+                          color: `rgba(255,255,255,${activeIcon ? 0.8 : 0.42})`
+                        }}
+                      />
+                      <Grid item className={classes.menuText}>
+                        <Typography
+                          variant={'subtitle2'}
+                          style={{
+                            color: `rgba(255,255,255,${activeIcon ? 1 : 0.8})`
+                          }}
+                        >
+                          {title}
+                        </Typography>
+                      </Grid>
+                    </Grid>
+                  </>
+                )}
               </MenuItem>
             ) : null}
           </>
-        )
-      )}
+        );
+      })}
       {/* Choose date */}
     </>
   );
