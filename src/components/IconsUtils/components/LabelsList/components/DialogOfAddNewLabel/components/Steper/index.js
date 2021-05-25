f081caa545651d3a9fadc7ab2a135ce48f59acd1
pas <whyiampas@gmail.com>
2021-05-26 00:09:43 +0300

    added base 2 components of steper and some base structure maded

--- src/components/IconsUtils/components/LabelsList/components/DialogOfAddNewLabel/components/Steper/index.js
+++ src/components/IconsUtils/components/LabelsList/components/DialogOfAddNewLabel/components/Steper/index.js
@@ -1,31 +1,56 @@
 import PropTypes from 'prop-types';
-import { makeStyles, Button, Stepper, StepLabel, StepContent, Step } from '@material-ui/core';
+import { makeStyles, Button, Stepper, StepLabel, StepContent, Step, Grid } from '@material-ui/core';
 import { useCounter } from 'react-use';
 
-const useStyles = makeStyles(theme => ({}));
-
+const useStyles = makeStyles(({ spacing }) => ({
+  container: {
+    '& .MuiCollapse-wrapperInner': {
+      display: 'flex',
+      alignItems: 'center'
+    }
+  },
+  buttonContainer: {
+    margin: spacing(0, 0, 0, 1),
+    '& button': {
+      float: 'right'
+    }
+  },
+  componentContainer: {
+    minWidth: spacing(32)
+  }
+}));
 const SteperOfDialogOfAddNewLabel = ({ stepsArrOfDialogOfAddNewLabel }) => {
   const classes = useStyles();
 
   const [activeStep, { inc: incrementActiveStep, dec: decrimentActiveStep, set, reset }] = useCounter(0);
 
   return (
-    <Stepper activeStep={activeStep} orientation={'vertical'}>
-      {stepsArrOfDialogOfAddNewLabel.map(({ title, component }) => (
-        <Step key={title}>
-          <StepLabel>{title} </StepLabel>
-          <StepContent>
-            {component}
-            <Button disabled={activeStep === 0} onClick={() => decrimentActiveStep()}>
-              Back
-            </Button>
-            <Button color={'primary'} onClick={() => incrementActiveStep()}>
-              {activeStep === stepsArrOfDialogOfAddNewLabel.length - 1 ? 'Finish' : 'Next'}
-            </Button>
-          </StepContent>
-        </Step>
-      ))}
-    </Stepper>
+    <Grid className={classes.container}>
+      <Stepper activeStep={activeStep} orientation={'vertical'}>
+        {stepsArrOfDialogOfAddNewLabel.map(({ title, component }) => (
+          <Step key={title}>
+            <StepLabel>{title} </StepLabel>
+            <StepContent>
+              <Grid container className={classes.componentContainer}>
+                {component}
+              </Grid>
+              <Grid container direction={'column'} className={classes.buttonContainer}>
+                <Grid>
+                  <Button disabled={activeStep === 0} onClick={() => decrimentActiveStep()} size={'small'}>
+                    Back
+                  </Button>
+                </Grid>
+                <Grid>
+                  <Button color={'secondary'} onClick={() => incrementActiveStep()} size={'small'}>
+                    {activeStep === stepsArrOfDialogOfAddNewLabel.length - 1 ? 'Finish' : 'Next'}
+                  </Button>
+                </Grid>
+              </Grid>
+            </StepContent>
+          </Step>
+        ))}
+      </Stepper>
+    </Grid>
   );
 };
 
--- src/components/IconsUtils/components/LabelsList/components/DialogOfAddNewLabel/index.js
+++ src/components/IconsUtils/components/LabelsList/components/DialogOfAddNewLabel/index.js
@@ -10,7 +10,9 @@ import {
   StepLabel,
   StepContent,
   Step,
-  Typography
+  Typography,
+  TextField,
+  FormControlLabel
 } from '@material-ui/core';
 import { themeColors } from 'components/theme';
 import PropTypes from 'prop-types';
@@ -19,13 +21,23 @@ import { connect } from 'react-redux';
 import SaveRoundedIcon from '@material-ui/icons/SaveRounded';
 import { handleAddNewGlobalLabelThunk } from 'store/modules/App/operations';
 import SteperOfDialogOfAddNewLabel from './components/Steper';
+import { Switch } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({}));
 
 const DialogOfAddNewLabel = ({ isDialogOpen, handleCloseAddNewLabelDialog, handleAddNewGlobalLabelThunk }) => {
   const classes = useStyles();
 
-  const [newLabelState, setNewLabelState] = useState();
+  const labelVariants = ['outlined', 'default'];
+
+  const nullityOfNewLabelState = {
+    title: '',
+    variant: labelVariants[0]
+  };
+  const [newLabelState, setNewLabelState] = useState(nullityOfNewLabelState);
+
+  const isLabelOutlined = newLabelState.variant === labelVariants[0];
+
 
   const handleCloseDialog = () => {
     console.log('log');
@@ -35,9 +47,34 @@ const DialogOfAddNewLabel = ({ isDialogOpen, handleCloseAddNewLabelDialog, handl
     console.log('log');
   };
 
+  const onChangeOfLabelTitleInput = ({ target: { value } }) => setNewLabelState(state => ({ ...state, title: value }));
+  const onChangeOfLabelVariantSwitch = () => {
+    setNewLabelState(state => ({ ...state, variant: isLabelOutlined ? labelVariants[1] : labelVariants[0] }));
+  };
+
   const stepsArrOfDialogOfAddNewLabel = [
-    { title: 'Enter the title', component: <div>Enter the title</div> },
-    { title: 'Chose a variant', component: <div>Chose a variant</div> },
+    {
+      title: 'Enter the title',
+      component: (
+        <TextField
+          required
+          label={'Required'}
+          variant={'outlined'}
+          color={'secondary'}
+          value={newLabelState.title}
+          onChange={onChangeOfLabelTitleInput}
+        />
+      )
+    },
+    {
+      title: 'Chose a variant',
+      component: (
+        <FormControlLabel
+          control={<Switch checked={isLabelOutlined} onChange={onChangeOfLabelVariantSwitch} />}
+          label={'Is variant Outlined? '}
+        />
+      )
+    },
     { title: 'Chose a color', component: <div>Chose a color</div> }
   ];
 
@@ -52,6 +89,7 @@ const DialogOfAddNewLabel = ({ isDialogOpen, handleCloseAddNewLabelDialog, handl
         <Button onClick={handleCloseDialog} style={{ color: themeColors.whiteRgbaColorWith0dot42valueOfAlfaCanal }}>
           Close
         </Button>
+
         <Button onClick={handleSave} color={'primary'} startIcon={<SaveRoundedIcon />}>
           Save
         </Button>
