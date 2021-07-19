cf8ea22c9e13777b8d6c5349be0e8c0a420ea5ed
pas <whyiampas@gmail.com>
2021-07-19 13:23:17 +0300

    ffff

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,13 +3,13 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
 import * as reducers from './modules';
 
-let configureStore = {};
+// let configureStore = {};
 
 const combinecRedusers = combineReducers({ ...reducers });
-if (process.env.NODE_ENV === 'production') {
-  configureStore = createStore(combinecRedusers,  compose(applyMiddleware(thunkMiddleware)));
-} else {
-  configureStore = createStore(combinecRedusers, composeWithDevTools(applyMiddleware(thunkMiddleware)));
-}
+// if (process.env.NODE_ENV === 'production') {
+const configureStore = createStore(combinecRedusers, compose(applyMiddleware(thunkMiddleware)));
+// } else {
+// configureStore = createStore(combinecRedusers, composeWithDevTools(applyMiddleware(thunkMiddleware)));
+// }
 
 export default configureStore;
