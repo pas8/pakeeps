7fb5c78f146d3c09709aa784c7774ea3f48a4934
pas <whyiampas@gmail.com>
2021-07-19 11:22:58 +0300

    ficking theme bugs

--- src/hooks/useTakeValueFromBreakpoints.hook.ts
+++ src/hooks/useTakeValueFromBreakpoints.hook.ts
@@ -1,10 +1,16 @@
 import { useTheme } from '@material-ui/core';
 import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { useMedia, useWindowSize } from 'react-use';
+import { getColor } from 'store/modules/Color/hooks';
 import { $Values } from 'utility-types';
 
-export const useTakeValueFromBreakpoints = (values:any[]  = ['xl', 'lg', 'md', 'sm', 'xs']) => {
-  const { breakpointsArr } = useTheme();
+export const useTakeValueFromBreakpoints = (values: any[] = ['xl', 'lg', 'md', 'sm', 'xs']) => {
+  const { breakpointsValues } = useSelector(getColor);
+
+  const { xs, sm, md, lg, xl } = breakpointsValues;
+  const breakpointsArr = [xl, lg, md, sm, xs];
+
   const [state, setState] = useState(values[0]);
   const { width } = useWindowSize();
 
