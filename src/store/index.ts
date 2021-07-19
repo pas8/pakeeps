5cc9d3af3d612a274d82bbbdd601d2530e31e460
pas <whyiampas@gmail.com>
2021-07-19 13:16:37 +0300

    fixed env problems

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,15 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
 import * as reducers from './modules';
 
-const store = createStore(combineReducers({ ...reducers }), composeWithDevTools(applyMiddleware(thunkMiddleware)));
+let configureStore = {};
 
-export default store;
+const combinecRedusers = combineReducers({ ...reducers });
+if (process.env.NODE_ENV === 'production') {
+  configureStore = createStore(combinecRedusers, {}, compose(applyMiddleware(thunkMiddleware)));
+} else {
+  configureStore = createStore(combinecRedusers, composeWithDevTools(applyMiddleware(thunkMiddleware)));
+}
+
+export default configureStore;
