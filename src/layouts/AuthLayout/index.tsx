0e7452c61e2358ee5087f9117f95e5faf5b89c98
pas <whyiampas@gmail.com>
2021-07-19 11:44:50 +0300

    env bug

--- src/layouts/AuthLayout/index.tsx
+++ src/layouts/AuthLayout/index.tsx
@@ -13,13 +13,13 @@ import { toChangeLoginStatus } from 'store/modules/Auth/actions';
 
 if (firebase.apps.length === 0)
   firebase.initializeApp({
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: process.env.GATSBY_API_KEY,
+    authDomain: process.env.GATSBY_AUTH_DOMAIN,
+    projectId: process.env.GATSBY_PROJECT_ID,
+    storageBucket: process.env.GATSBY_STORAGE_BUCKET,
+    messagingSenderId: process.env.GATSBY_MESSAGING_SENDER_ID,
+    appId: process.env.GATSBY_APP_ID,
+    measurementId: process.env.GATSBY_MEASUREMENT_ID
   });
 
 const AuthLayout: FC<any> = ({ children, pageProps }) => {
